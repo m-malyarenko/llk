ec39a0c1c9c235430fd4db91d8ba7ab65d2dff9b
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-11-06 14:04:22 +0300

    dev: main llk parser mod outline

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub mod error;
+pub mod grammar;
+pub mod tree;
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub use crate::error::LlkError;
+pub use crate::grammar::LlkGrammar;
+pub use crate::tree::LlkTree;
+
+pub struct Llk {
+    grammar: LlkGrammar,
+    lut: HashMap<(char, String), String>,
+    stack: RefCell<Vec<char>>,
+}
+
+impl Llk {
+    pub fn new() -> Llk {
+        unimplemented!()
+    }
+
+    pub fn parse(&self, string: &str) -> Result<LlkTree, LlkError> {
+        unimplemented!()
+    }
+
+    fn first(&self, string: &str) -> HashSet<&str> {
+        unimplemented!()
+    }
+
+    fn follow(&self, nterm: char) -> HashSet<&str> {
+        unimplemented!()
+    }
+}
