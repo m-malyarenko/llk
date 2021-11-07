5f06e56789b4b652588d3465c3a11c96348d2968
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-11-07 16:55:54 +0300

    dev: grammar data structure, parser support methods
    Productions LHS switched to Option<String> to support epsilon productions
    EOF character added
    First set generator support methods added

--- src/grammar.rs
+++ src/grammar.rs
@@ -4,12 +4,14 @@ use std::collections::HashSet;
 use crate::error::LlkError;
 
 pub struct LlkGrammar {
-    term_symbols: HashSet<char>,
-    nterm_symbols: HashSet<char>,
-    start_symbol: char,
-    lookahead: usize,
-    productions: HashMap<char, Vec<String>>,
+    pub term_symbols: HashSet<char>,
+    pub  nterm_symbols: HashSet<char>,
+    pub  start_symbol: char,
+    pub  lookahead: usize,
+    pub  productions: HashMap<char, Vec<Option<String>>>,
 }
+
+pub(super) const EOF: char = '\u{0003}';
 
 impl LlkGrammar {
     pub fn new(
@@ -17,7 +19,7 @@ impl LlkGrammar {
         nterm_symbols: HashSet<char>,
         start_symbol: char,
         lookahead: usize,
-        productions: Vec<(char, String)>,
+        productions: Vec<(char, Option<String>)>,
     ) -> Result<LlkGrammar, LlkError> {
         unimplemented!()
     }
@@ -27,10 +29,79 @@ impl LlkGrammar {
     }
 }
 
+impl LlkGrammar {
+    pub(super) fn is_term(&self, symbol: char) -> bool {
+        self.term_symbols.contains(&symbol) || symbol == EOF
+    }
+
+    pub(super) fn is_nterm(&self, symbol: char) -> bool {
+        self.nterm_symbols.contains(&symbol) || self.start_symbol == symbol
+    }
+
+    pub(super) fn derives_epsilon(&self, symbol: char) -> bool {
+        !self.is_term(symbol) && self.productions[&symbol].contains(&None)
+    }
+
+    pub(super) fn derive(&self, symbol: char) -> Vec<Option<String>> {
+        if self.is_nterm(symbol) {
+            self.productions[&symbol].clone()
+        } else {
+            vec![Some(symbol.to_string())]
+        }
+    }
+
+    pub(super) fn get_term_prefixes(&self, string: &str) -> HashSet<String> {
+        /* Recursive expansion procedure */
+        fn inner(grammar: &LlkGrammar, string: &str, prefix_len: usize) -> Vec<String> {
+            if let Some(leftmost_nterm) = string.chars().find(|&c| grammar.is_nterm(c)) {
+                /* String contains at least one non-terminal symbol */
+                let (prefix, suffix) = string.split_once(leftmost_nterm).unwrap();
+
+                if prefix.len() >= prefix_len {
+                    /* If prefix length is enough return it */
+                    vec![format!(
+                        "{term_prefix:.limit$}",
+                        term_prefix = prefix,
+                        limit = prefix_len
+                    )]
+                } else {
+                    let prefix_rest_len = prefix_len - prefix.len();
+
+                    grammar
+                        .derive(leftmost_nterm)
+                        .drain(..)
+                        .map(|d| d.unwrap_or_default())
+                        .filter(|s| !s.starts_with(leftmost_nterm)) // TODO Сомнительное рещение для предотвращения беск. рекурсии
+                        .flat_map(|s| {
+                            inner(grammar, &(s + suffix), prefix_rest_len)
+                        })
+                        .map(|s| {
+                            format!(
+                                "{prefix_start}{prefix_end}",
+                                prefix_start = prefix,
+                                prefix_end = s,
+                            )
+                        })
+                        .collect()
+                }
+            } else {
+                /* If string contains only terminal symbols return its k-prefix */
+                vec![format!(
+                    "{term_prefix:.limit$}",
+                    term_prefix = string,
+                    limit = prefix_len
+                )]
+            }
+        }
+
+        inner(self, string, self.lookahead).drain(..).collect()
+    }
+}
+
 mod grammar_assert {
-    use std::collections::HashSet;
     use super::LlkGrammar;
     use crate::error::LlkError;
+    use std::collections::HashSet;
 
     fn assert_grammar(grammar: &LlkGrammar) -> Result<(), LlkError> {
         unimplemented!()
