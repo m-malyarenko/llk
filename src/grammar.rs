01e3e756e1075dda744cb630ae7fddf8e1a32eec
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-11-07 17:03:35 +0300

    refactor: first & follow moved to grammar

--- src/grammar.rs
+++ src/grammar.rs
@@ -5,10 +5,10 @@ use crate::error::LlkError;
 
 pub struct LlkGrammar {
     pub term_symbols: HashSet<char>,
-    pub  nterm_symbols: HashSet<char>,
-    pub  start_symbol: char,
-    pub  lookahead: usize,
-    pub  productions: HashMap<char, Vec<Option<String>>>,
+    pub nterm_symbols: HashSet<char>,
+    pub start_symbol: char,
+    pub lookahead: usize,
+    pub productions: HashMap<char, Vec<Option<String>>>,
 }
 
 pub(super) const EOF: char = '\u{0003}';
@@ -27,22 +27,90 @@ impl LlkGrammar {
     pub fn from_json(json_string: &str) -> Result<LlkGrammar, LlkError> {
         unimplemented!()
     }
+
+    pub fn first(&self, string: &str) -> HashSet<Option<String>> {
+        let mut first_set = HashSet::new();
+
+        if string.is_empty() {
+            /* Calculate FIRST set of ε */
+            first_set.insert(None);
+        } else if string.len() == 1 {
+            /* Calculate FIRST set of a symbol */
+            let symbol = string.chars().nth(0).unwrap();
+
+            if self.is_term(symbol) {
+                /* FIRST set of terminal symbol is itself */
+                first_set.insert(Some(string.to_owned()));
+            } else {
+                /* FIRST set of non-termonal symbol */
+
+                /* If non-terminal symbol derives ε then add ε to its FIRST set */
+                if self.derives_epsilon(symbol) {
+                    first_set.insert(None);
+                }
+
+                /* Get all non-terminal non-ε derivatives */
+                let derivatives: Vec<String> =
+                    self.derive(symbol).drain(..).filter_map(|d| d).collect();
+
+                /* Scan through all derivatives */
+                for derivative in derivatives {
+                    /* Skip symbols that have ε in their FIRST set */
+                    let non_empty_suffix = if let Some(suffix) = derivative
+                        .strip_prefix(|c: char| self.first(&c.to_string()).contains(&None))
+                    {
+                        suffix
+                    } else {
+                        &derivative
+                    };
+
+                    if non_empty_suffix.is_empty() {
+                        first_set.insert(None);
+                    } else {
+                        /* Get all k-prefixes of non-ε suffix of the production's RHS */
+                        first_set.extend(
+                            self.get_term_prefixes(non_empty_suffix)
+                                .drain()
+                                .map(|s| Some(s)),
+                        )
+                    }
+                }
+            }
+        } else {
+            /* Calculate FIRST set of a string */
+            /* Most often used to define the FIRST set of the RHS of a production */
+            if string
+                .chars()
+                .all(|symbol| self.first(&symbol.to_string()).contains(&None))
+            {
+                first_set.insert(None);
+            };
+
+            first_set.extend(self.get_term_prefixes(string).drain().map(|s| Some(s)))
+        }
+
+        first_set
+    }
+
+    pub fn follow(&self, nterm: char) -> HashSet<String> {
+        unimplemented!()
+    }
 }
 
 impl LlkGrammar {
-    pub(super) fn is_term(&self, symbol: char) -> bool {
+    fn is_term(&self, symbol: char) -> bool {
         self.term_symbols.contains(&symbol) || symbol == EOF
     }
 
-    pub(super) fn is_nterm(&self, symbol: char) -> bool {
+    fn is_nterm(&self, symbol: char) -> bool {
         self.nterm_symbols.contains(&symbol) || self.start_symbol == symbol
     }
 
-    pub(super) fn derives_epsilon(&self, symbol: char) -> bool {
+    fn derives_epsilon(&self, symbol: char) -> bool {
         !self.is_term(symbol) && self.productions[&symbol].contains(&None)
     }
 
-    pub(super) fn derive(&self, symbol: char) -> Vec<Option<String>> {
+    fn derive(&self, symbol: char) -> Vec<Option<String>> {
         if self.is_nterm(symbol) {
             self.productions[&symbol].clone()
         } else {
@@ -50,7 +118,7 @@ impl LlkGrammar {
         }
     }
 
-    pub(super) fn get_term_prefixes(&self, string: &str) -> HashSet<String> {
+    fn get_term_prefixes(&self, string: &str) -> HashSet<String> {
         /* Recursive expansion procedure */
         fn inner(grammar: &LlkGrammar, string: &str, prefix_len: usize) -> Vec<String> {
             if let Some(leftmost_nterm) = string.chars().find(|&c| grammar.is_nterm(c)) {
@@ -72,9 +140,7 @@ impl LlkGrammar {
                         .drain(..)
                         .map(|d| d.unwrap_or_default())
                         .filter(|s| !s.starts_with(leftmost_nterm)) // TODO Сомнительное рещение для предотвращения беск. рекурсии
-                        .flat_map(|s| {
-                            inner(grammar, &(s + suffix), prefix_rest_len)
-                        })
+                        .flat_map(|s| inner(grammar, &(s + suffix), prefix_rest_len))
                         .map(|s| {
                             format!(
                                 "{prefix_start}{prefix_end}",
@@ -126,4 +192,65 @@ mod grammar_assert {
     fn get_resolvable_nterms(grammar: &LlkGrammar) -> HashSet<char> {
         unimplemented!()
     }
+}
+
+#[test]
+fn first_set_test() {
+    use std::iter::FromIterator;
+
+    let mut grammar = LlkGrammar {
+        term_symbols: vec!['a', 'b', '$'].drain(..).collect(),
+        nterm_symbols: vec!['A'].drain(..).collect(),
+        start_symbol: 'S',
+        lookahead: 3,
+        productions: HashMap::new(),
+    };
+
+    grammar
+        .productions
+        .insert('S', vec![Some("Ab$".to_string())]);
+    grammar
+        .productions
+        .insert('A', vec![Some("aA".to_string()), Some("a".to_string())]);
+
+    assert_eq!(
+        grammar.first("S"),
+        HashSet::from_iter(vec![
+            Some("ab$".to_string()),
+            Some("aab".to_string()),
+            Some("aaa".to_string())
+        ])
+    );
+    assert_eq!(
+        grammar.first("A"),
+        HashSet::from_iter(vec![
+            Some("a".to_string()),
+            Some("aa".to_string()),
+            Some("aaa".to_string())
+        ])
+    );
+    assert_eq!(
+        grammar.first("a"),
+        HashSet::from_iter(vec![Some("a".to_string())])
+    );
+    assert_eq!(
+        grammar.first("b"),
+        HashSet::from_iter(vec![Some("b".to_string())])
+    );
+    assert_eq!(
+        grammar.first("$"),
+        HashSet::from_iter(vec![Some("$".to_string())])
+    );
+    assert_eq!(
+        grammar.first("Ab$"),
+        HashSet::from_iter(vec![
+            Some("ab$".to_string()),
+            Some("aab".to_string()),
+            Some("aaa".to_string())
+        ])
+    );
+    assert_eq!(
+        grammar.first("aA"),
+        HashSet::from_iter(vec![Some("aaa".to_string()), Some("aa".to_string())])
+    );
 }
