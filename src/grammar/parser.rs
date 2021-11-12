d486d456fe838520e5fb6b6829663f74e12af260
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-11-12 11:34:55 +0300

    dev: dervation tree building implemented

--- src/grammar/parser.rs
+++ src/grammar/parser.rs
@@ -21,9 +21,14 @@ impl LlkParser {
         let target_string: String = format!("{}{}", string, LlkGrammar::EOF);
         let lookahead_len = self.grammar.lookahead;
         let mut stack = Vec::new();
+        let mut tree_root = LlkTree::new(self.grammar.start_symbol);
+        let mut tree_node_stack = Vec::new();
 
         /* Init stack with the start symbol */
         stack.push(self.grammar.start_symbol);
+
+        /* Init tree node stack with root tree */
+        tree_node_stack.push(&mut tree_root as *mut LlkTree);
 
         for start_symbol_idx in 0..string.len() {
             let rest_target_string = &target_string[start_symbol_idx..];
@@ -32,28 +37,45 @@ impl LlkParser {
             /* Get the top of the stack */
             let top = *stack.last().unwrap();
 
-            /* If stack top is terminal symbol */
+            /*
+             * If stack top is terminal symbol == stack top
+             * pop stack top symbol and continue
+             */
             if self.grammar.is_term(top) && top == current_term {
                 stack.pop();
-                continue;
-            }
-
-            /* If stack top is non-terminal symbol */
-            let lookahead = if rest_target_string.len() >= lookahead_len {
-                rest_target_string[..lookahead_len].to_owned()
             } else {
-                rest_target_string[..].to_owned()
-            };
+                let lookahead = if rest_target_string.len() >= lookahead_len {
+                    rest_target_string[..lookahead_len].to_owned()
+                } else {
+                    rest_target_string[..].to_owned()
+                };
 
-            if let Some(production_rhs) = self.lut.get(&(top, lookahead)) {
-                stack.pop();
-                stack.extend(production_rhs.chars().rev());
-            } else {
-                return Err(LlkError::DerivationFailed(String::default())); // FIXME Исправить сообщение ошибки
+                if let Some(production_rhs) = self.lut.get(&(top, lookahead)) {
+                    /* Push production RHS to the stack */
+                    stack.extend(production_rhs.chars().rev());
+
+                    /* Update derivation tree */
+                    let tree_node_stack_top = tree_node_stack.pop().unwrap();
+
+                    for symbol in production_rhs.chars().rev() {
+                        if self.grammar.is_nterm(symbol) {
+                            unsafe {
+                                let new_node = (*tree_node_stack_top).push_node(symbol);
+                                tree_node_stack.push(new_node);
+                            }
+                        } else {
+                            unsafe {
+                                (*tree_node_stack_top).push_leaf(symbol);
+                            }
+                        }
+                    }
+                } else {
+                    return Err(LlkError::DerivationFailed(String::default())); // FIXME Исправить сообщение ошибки
+                }
             }
         }
 
-        unimplemented!()
+        Ok(tree_root)
     }
 }
 
